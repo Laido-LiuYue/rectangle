e067e64ad8712a5f61390c557149155c27e8eb11
Laido-LiuYue <578201842@qq.com>
2019-03-19 13:17:10 +0800

    rectanglev0.3 sthwrong

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,15 +15,26 @@ module.exports = function (grunt) {
     },
    htmlhint: {
       options: {
-        htmlhintrc: '.htmlhintrc'    
+      htmlhintrc: '.htmlhintrc'    
     },
       src: '*.html'    
-    }
+    },
+   mocha: {
+      test: {
+        src: ['test/index.html'],                 
+    },
+    options: {
+      run: true,
+      reporter: 'Spec'                            
+    }       
+   }
   });
 
   grunt.loadNpmTasks('grunt-contrib-csslint');
   grunt.loadNpmTasks('grunt-htmlhint');
   grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-mocha');
 
   grunt.registerTask('default', ['htmlhint', 'csslint', 'eslint']);
+  grunt.registerTask('unitTest', ['mocha']);
 };
--- rectangle.js
+++ rectangle.js
@@ -1,17 +1,23 @@
-$(function(){
-  var $width = $('#width'),
+/* global Rectangle: true */
+$(function() {
+    var $width = $('#width'),
       $height = $('#height'),
       $btnCal = $('#calculate'),
       $perimeter = $('#perimeter'),
       $area = $('#area');
 
-  $btnCal.click(function(){
+$btnCal.click(function(){
     var w = Number($width.val()),
         h = Number($height.val());
-    var p = 2 * (w+h),
-      a = w * h;
+    var w = $width.val(),
+        h = $height.val();
+    var p = 2 * (w + h),
+        a = w * h;
+    var r = new Rectangle(w, h);
+        $perimeter.val(p);
+        $area.val(a);
+        $perimeter.val(r.perimeter());
+        $area.val(r.area());                    
+  });
 
-    $perimeter.val(p);
-    $area.val(a);
-  }
-})
+});
